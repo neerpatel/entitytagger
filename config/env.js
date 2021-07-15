76e68f5dd0f1c6632009a601752786a1083bcaa2
Neer Patel <neerpatel@gmail.com>
2021-07-15 22:02:37 +0000

    Added License file, Added support for .env files, andded getfilelist & load file endpoints

---
+++ config/env.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+
+module.exports = {
+    dataPath: process.env.DATA_PATH,
+    tagsDocPath: process.env.TAGS_DOC_PATH
+};
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+
 const path = require('path');
 const randomColor = require('randomcolor');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const app = express();
+
+const env = require("./config/env");
+const junk = require('junk');
 
 function rndColor(arr) {
     var color = randomColor({luminosity: 'dark'});
@@ -18,9 +22,21 @@ isObject = function(a) {
 app.use("/",express.static("./html"));
 app.use(bodyParser.json());
 
+app.get("/getfilelist", (req, res) => {
+    var response = [];
+    var files = fs.readdirSync(env.dataPath, { withFileTypes: true });
+    files.forEach(function (file) {
+        if (file.isFile() && (/(.*)\.txt/g).test(file.name)) {
+            response.push({filename: file.name});
+        }
+    });
+    res.send(response);
+});
+
+
 app.get("/tags", (req, res) => {
     var response = [];
-    var tagData = JSON.parse(fs.readFileSync('./data/tags.json', 'utf8'));
+    var tagData = JSON.parse(fs.readFileSync(env.tagsDocPath, 'utf8'));
 
     for(var tag in tagData.tags){
         response.push({text: tagData.tags[tag], color: rndColor(response)} );
@@ -28,9 +44,14 @@ app.get("/tags", (req, res) => {
     res.send(response);
 });
 
-app.get("/text", (req, res) => {
-    var response = fs.readFileSync('./data/text.txt', 'utf8');
-    res.send(response);
+app.post("/loadfile", (req, res) => {
+    if (req.body.filename != undefined && fs.existsSync(env.dataPath + '/' + req.body.filename) ) {
+        var response = fs.readFileSync(env.dataPath + '/' + req.body.filename, 'utf8');
+        res.send(response);
+    } else {
+        res.status(404).json({ error: "Data File Not Found"});
+    }
+    
 });
 app.get("/spacy", (req, res) => {
     var response = fs.readFileSync('./data/spacy.txt', 'utf8');
